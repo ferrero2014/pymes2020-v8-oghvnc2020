85bc329fc5f6b31291c25a962e53830b8af9f215
ferrero2014 <ferrerocarina@gmail.com>
2020-11-23 07:36:22 -0300

    1 file added

---
+++ src/app/models/empresa.ts
@@ -0,0 +1,44 @@
+export class Empresa {
+IdEmpresa: number;
+  Nombre: string
+  FechaFundacion: string;
+  CantidadEmpleados: number;
+  };
+export const Articulos: Articulo[] = [
+  {
+    IdEmpresa: 108,
+    Nombre: "Adaptador usb wifi tl-wn722n",
+    FechaFundacion: "2017-01-23T00:00:00",
+    Stock: 898,
+    },
+  {
+    IdEmpresa: 109,
+    Nombre: "Adaptador usb wifi tl-wn722n",
+    FechaFundacion: "2017-01-23T00:00:00",
+    Stock: 898,
+    },
+  {
+    IdEmpresa: 110,
+    Nombre: "Adaptador usb wifi tl-wn722n",
+    FechaFundacion: "2017-01-23T00:00:00",
+    Stock: 898,
+    },
+  {
+    IdEmpresa: 111,
+    Nombre: "Adaptador usb wifi tl-wn722n",
+    FechaFundacion: "2017-01-23T00:00:00",
+    Stock: 898,
+    },
+  {
+    IdEmpresa: 112,
+    Nombre: "Adaptador usb wifi tl-wn722n",
+    FechaFundacion: "2017-01-23T00:00:00",
+    Stock: 898,
+    },
+  {
+    IdEmpresa: 114,
+    Nombre: "Adaptador usb wifi tl-wn722n",
+    FechaFundacion: "2017-01-23T00:00:00",
+    Stock: 898,
+    },
+];
